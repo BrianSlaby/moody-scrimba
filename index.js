2707dbb1d4e399c445e1f22490093907bc64660a
Brian Slaby <brian.slaby@gmail.com>
2023-12-03 21:36:21 -0500

    added functionality to filter posts by day, week, month, all

--- index.js
+++ index.js
@@ -65,6 +65,10 @@ const moodEmojiEls = document.getElementsByClassName("mood-emoji-btn")
 const textareaEl = document.getElementById("post-input")
 const postButtonEl = document.getElementById("post-btn")
 
+const allFilterButtonEl = document.getElementById("all-filter-btn")
+
+const filterButtonEls = document.getElementsByClassName("filter-btn")
+
 const postsEl = document.getElementById("posts")
 
 /* == UI - Event Listeners == */
@@ -78,6 +82,10 @@ signOutButtonEl.addEventListener("click", authSignOut)
 
 for (let moodEmojiEl of moodEmojiEls) {
     moodEmojiEl.addEventListener("click", selectMood)
+}
+
+for (let filterButtonEl of filterButtonEls) {
+    filterButtonEl.addEventListener("click", selectFilter)
 }
 
 updateProfileButtonEl.addEventListener("click", authUpdateProfile)
@@ -95,7 +103,8 @@ onAuthStateChanged(auth, (user) => {
         showLoggedInView()
         showProfilePicture(userProfilePictureEl, user)
         showUserGreeting(userGreetingEl, user)
-        fetchInRealtimeAndRenderPostsFromDB(user)
+        updateFilterButtonStyle(allFilterButtonEl)
+        fetchAllPosts(user)
     } else {
         showLoggedOutView()
     }
@@ -196,16 +205,80 @@ async function addPostToDB(postBody, user) {
       }
 }
 
-function fetchInRealtimeAndRenderPostsFromDB(user) {
-   const postsRef = collection(db, "posts")
-   const q = query(postsRef, where("uid", "==", user.uid), orderBy("createdAt", "desc"))
-
-    onSnapshot(q, (querySnapshot) => {
+function fetchInRealtimeAndRenderPostsFromDB(query) {
+    onSnapshot(query, (querySnapshot) => {
         clearAll(postsEl)
         querySnapshot.forEach(doc => {
             renderPost(postsEl, doc.data())
         })
     })
+}
+
+function fetchTodayPosts(user) {
+    const startOfDay = new Date()
+    startOfDay.setHours(0, 0, 0, 0)
+    
+    const endOfDay = new Date()
+    endOfDay.setHours(23, 59, 59, 999)
+    
+    const postsRef = collection(db, "posts")
+    
+    const q = query(postsRef, where("uid", "==", user.uid),
+                              where("createdAt", ">=", startOfDay),
+                              where("createdAt", "<=", endOfDay),
+                              orderBy("createdAt", "desc"))
+                              
+    fetchInRealtimeAndRenderPostsFromDB(q)                  
+}
+
+function fetchWeekPosts(user) {
+    const startOfWeek = new Date()
+    startOfWeek.setHours(0, 0, 0, 0)
+    
+    if (startOfWeek.getDay() === 0) { // If today is Sunday
+        startOfWeek.setDate(startOfWeek.getDate() - 6) // Go to previous Monday
+    } else {
+        startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() + 1)
+    }
+
+    const endOfDay = new Date()
+    endOfDay.setHours(23, 59, 59, 999)
+
+    const postsRef = collection(db, "posts")
+
+    const q = query(postsRef, where("uid", "==", user.uid),
+                              where("createdAt", ">=", startOfWeek),
+                              where("createdAt", "<=", endOfDay),
+                              orderBy("createdAt", "desc"))
+
+    fetchInRealtimeAndRenderPostsFromDB(q)
+}
+
+function fetchMonthPosts(user) {
+    const startOfMonth = new Date()
+    startOfMonth.setHours(0, 0, 0, 0)
+    startOfMonth.setDate(1)
+
+    const endOfDay = new Date()
+    endOfDay.setHours(23, 59, 59, 999)
+
+	const postsRef = collection(db, "posts")
+    
+    const q = query(postsRef, where("uid", "==", user.uid),
+                              where("createdAt", ">=", startOfMonth),
+                              where("createdAt", "<=", endOfDay),
+                              orderBy("createdAt", "desc"))
+
+    fetchInRealtimeAndRenderPostsFromDB(q)
+}
+
+function fetchAllPosts(user) {
+    const postsRef = collection(db, "posts")
+
+    const q = query(postsRef, where("uid", "==", user.uid), 
+                              orderBy("createdAt", "desc"))
+
+    fetchInRealtimeAndRenderPostsFromDB(q)
 }
 
 /* == Functions - UI Functions == */
@@ -342,4 +415,44 @@ function resetAllMoodElements(allMoodElements) {
 
 function returnMoodValueFromElementId(elementId) {
     return Number(elementId.slice(5))
+}
+
+/* == Functions - UI Functions - Date Filters == */
+
+function resetAllFilterButtons(allFilterButtons) {
+    for (let filterButtonEl of allFilterButtons) {
+        filterButtonEl.classList.remove("selected-filter")
+    }
+}
+
+function updateFilterButtonStyle(element) {
+    element.classList.add("selected-filter")
+}
+
+function fetchPostsFromPeriod(period, user) {
+    if (period === "today") {
+        fetchTodayPosts(user)
+    } else if (period === "week") {
+        fetchWeekPosts(user)
+    } else if (period === "month") {
+        fetchMonthPosts(user)
+    } else {
+        fetchAllPosts(user)
+    }
+}
+
+function selectFilter(event) {
+    const user = auth.currentUser
+    
+    const selectedFilterElementId = event.target.id
+    
+    const selectedFilterPeriod = selectedFilterElementId.split("-")[0]
+    
+    const selectedFilterElement = document.getElementById(selectedFilterElementId)
+    
+    resetAllFilterButtons(filterButtonEls)
+    
+    updateFilterButtonStyle(selectedFilterElement)
+
+    fetchPostsFromPeriod(selectedFilterPeriod, user)
 }
